edf979e5117fcd0da00c43025da92abf2f2dad8d
Seralius <develop.seralius@outlook.com>
2022-05-18 01:03:42 +0200

    Changed stuff

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -3,64 +3,34 @@ using Dalamud.IoC;
 using Dalamud.Plugin;
 using System.IO;
 using System.Reflection;
+using Dalamud.Game.Gui.ContextMenus;
 
-namespace SamplePlugin
+namespace CopyItemName
 {
     public sealed class Plugin : IDalamudPlugin
     {
-        public string Name => "Sample Plugin";
-
-        private const string commandName = "/pmycommand";
-
-        private DalamudPluginInterface PluginInterface { get; init; }
-        private CommandManager CommandManager { get; init; }
-        private Configuration Configuration { get; init; }
-        private PluginUI PluginUi { get; init; }
-
-        public Plugin(
-            [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
-            [RequiredVersion("1.0")] CommandManager commandManager)
+        [PluginService]
+        private ContextMenu ContextMenu { get; init; }
+        public string Name => "CopyItemName";
+        public Plugin()
         {
-            this.PluginInterface = pluginInterface;
-            this.CommandManager = commandManager;
+            System.IO.File.AppendAllText(@"C:\Users\MainPC\source\repos\DalamudCopyItemname\Log\log.txt", "Started!");
+            ContextMenu.ContextMenuOpened += ContextMenu_ContextMenuOpened;
+            System.IO.File.AppendAllText(@"C:\Users\MainPC\source\repos\DalamudCopyItemname\Log\log.txt", "Appended!");
+        }
 
-            this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
-            this.Configuration.Initialize(this.PluginInterface);
-
-            // you might normally want to embed resources and load them from the manifest stream
-            var imagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
-            var goatImage = this.PluginInterface.UiBuilder.LoadImage(imagePath);
-            this.PluginUi = new PluginUI(this.Configuration, goatImage);
-
-            this.CommandManager.AddHandler(commandName, new CommandInfo(OnCommand)
+        private void ContextMenu_ContextMenuOpened(ContextMenuOpenedArgs args)
+        {
+            System.IO.File.AppendAllText(@"C:\Users\MainPC\source\repos\DalamudCopyItemname\Log\log.txt", "Registered...");
+            args.AddCustomItem("Testing123", (_) =>
             {
-                HelpMessage = "A useful message to display in /xlhelp"
+                System.IO.File.AppendAllText(@"C:\Users\MainPC\source\repos\DalamudCopyItemname\Log\log.txt", "Clicked...");
             });
-
-            this.PluginInterface.UiBuilder.Draw += DrawUI;
-            this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
         }
 
         public void Dispose()
         {
-            this.PluginUi.Dispose();
-            this.CommandManager.RemoveHandler(commandName);
-        }
 
-        private void OnCommand(string command, string args)
-        {
-            // in response to the slash command, just display our main ui
-            this.PluginUi.Visible = true;
-        }
-
-        private void DrawUI()
-        {
-            this.PluginUi.Draw();
-        }
-
-        private void DrawConfigUI()
-        {
-            this.PluginUi.SettingsVisible = true;
         }
     }
 }
